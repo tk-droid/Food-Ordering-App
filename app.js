9d69ce9d59cde01dd532af61f55716336aecb3c1
tk-droid <taifk817@gmail.com>
2024-08-01 18:01:55 +0530

    EP1

---
+++ app.js
@@ -0,0 +1,8 @@
+const parent = React.createElement("div",{id:"parent"}, [React.createElement("div", {id:"child"},[React.createElement("h1", {}, "This is H1 Tag"), React.createElement("h2", {}, "This is H2 Tag")] 
+)], React.createElement("div", {id:"child"},[React.createElement("h1", {}, "This is H1 Tag"), React.createElement("h2", {}, "This is H2 Tag")] 
+))
+
+console.log(parent)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent)
