e4154d5c996fb1e32103f4e0e29ff96e032a55b8
MOHD TAIF KHAN <taifk817@gmail.com>
2024-12-14 10:22:36 +0530

    Delete src directory

--- src/components/__tests__/sum.test.js
+++
@@ -1,9 +0,0 @@
-import { sum } from "../sum";
-
-
-test ("Test to see addition of two numbers", ()=>{
-      const result = sum(1, 2);
-      
-      //Assertion
-      expect(result).toBe(3)
-});
--- src/utils/UserContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-const UserContext = createContext({
-    loggedInUser: "Default user is displayed because no value={} is given, if it was given than the value wouldve changed",
-});
-
-export default UserContext
